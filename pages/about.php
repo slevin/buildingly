fb2dbf84f329b9b1c9352b4e9c7337ba6ff66606
Alessandra Noelting <alnova@Alessandras-Doomsday-Device.local>
2011-11-05 21:09:08 -0400

    adding pages

---
+++ pages/about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Buildingly</title>
+    <meta name="description" content="buildingly">
+    <meta name="author" content="buildingly">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="../bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">Buildingly</a>
+          <ul class="nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+      <img src="../assets/Buildingly_logo.png" alt="Buildingly" />
+        <h2>What's happening in your building?</h2>
+        <p>You spend 90% of your time in the same two or three places. See what's happening and who's doing what in your building!</p>
+        <p><a class="btn primary large">Get Started &raquo;</a></p>
+      </div>
+
+
+
+  </body>
+</html>
---
+++ pages/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Buildingly</title>
+    <meta name="description" content="buildingly">
+    <meta name="author" content="buildingly">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="../bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">Buildingly</a>
+          <ul class="nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+      <img src="../assets/Buildingly_logo.png" alt="Buildingly" />
+        <h2>What's happening in your building?</h2>
+        <p>You spend 90% of your time in the same two or three places. See what's happening and who's doing what in your building!</p>
+        <p><a class="btn primary large">Get Started &raquo;</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+      
+        <div class="span-one-third">
+          <h2>Food Delivery</h2>
+          <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+          <p><a class="btn primary large" href="#">View Details &raquo;</a></p>
+        </div>
+        
+        <div class="span-one-third">
+          <h2>Deals</h2>
+          
+          <h3>Deal Title</h3>
+          
+          <h4>Business Name</h4>
+          <p>Street Address</p>
+           
+           <h4>$4.99</h4>
+           
+           <div align="left" style="border:1px solid black">Worth: $14.99  |  Discount: 67%  |  Savings: $10</div>
+           
+           <br/>
+           
+          <div align="right"<p><a class="btn primary large" href="#">Grab it! &raquo;</a></p></div>
+          <div align="right" style="border:1px black">Time left: 1hr <br/></div>
+          
+       </div>
+       
+        <div class="span-one-third">
+          <h2>Social Feed</h2>
+          <h3>Facebook</h3>
+          <p>News Feed News Feed News Feed News Feed News Feed News Feed News Feed News Feed News Feed News Feed </p>
+          <p><a class="btn primary large" href="#">See more &raquo;</a></p>
+        </div>
+      </div>
+
+      <footer>
+        <p>&copy; Buildingly, 2011</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
