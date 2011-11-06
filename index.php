122fb2a0b857abb7a4cfb8ef05a76b15bb18f556
Sean Levin <sean.levin@gmail.com>
2011-11-06 13:53:09 -0500

    more fix

--- index.php
+++ index.php
@@ -18,74 +18,8 @@
         padding-top: 60px;
       }
     </style>
+
     <style>
-div.loading{background-image:url('http://www.careeravenues.in/Images/loadingGIF.gif');height:10em; width:10em;background-repeat:no-repeat;}
-</style>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.js"></script>
-<?php
-session_start();
-$address = isset($_GET['address']) ? $_GET['address'] : '';
-$city = isset($_GET['city']) ? $_GET['city'] : 'New York';
-
-if ($address == ''):
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Buildingly</title>
-    <meta name="description" content="buildingly">
-    <meta name="author" content="buildingly">
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- Le styles -->
-    <link href="/bootstrap.css" rel="stylesheet">
-    <link href="/buldingly.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-      }
-    </style>
-    <style>
-div.loading{background-image:url('http://www.careeravenues.in/Images/loadingGIF.gif');height:10em; width:10em;background-repeat:no-repeat;}
-</style>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.js"></script>
-	<script>
-			$(function(){
-                                        $.ajax({url:"bing_test.php?address=<?php echo $address; ?>&city=<?php echo $city; ?>",
-                                        dataType:'text',
-                                        success:function(data){getlocalinfo(data.split(':'));}});
-			});
-
-function getlocalinfo(arrLatLon){
-					$.ajax({url:"foursquare_test.php?lat="+arrLatLon[0]+"&long="+arrLatLon[1], 
-					dataType:'html',
-					success:function(data){$('#foursquare').append(data); $('#loadingfoursquare').css('display', 'none');}});
-
-					$.ajax({url:"yipit_test.php?lat="+arrLatLon[0]+"&long="+arrLatLon[1],
-					dataType:'html',
-					success:function(data){$('#yipit').append(data); $('#loadingyipit').css('display', 'none');}});
-
-					$.ajax({url:"ordr_test.php", 
-					dataType:'html',
-					success:function(data){$('#ordr').append(data); $('#loadingordr').css('display', 'none');}});
-
-					$.ajax({url:"meetup_test.php?lat="+arrLatLon[0]+"&long="+arrLatLon[1],
-					dataType:'html',
-					success:function(data){$('#meetup').append(data); $('#loadingmeetup').css('display', 'none');}});
-}
-		</script>
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="images/favicon.ico">
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
-
-      <style>
          .header_bar {
     height:176px;
     background-image: url('/assets/header_bar.png');
@@ -168,6 +102,52 @@ function getlocalinfo(arrLatLon){
               font: 12pt Helvetica, arial;
           }
       </style>
+
+
+
+    <style>
+div.loading{background-image:url('http://www.careeravenues.in/Images/loadingGIF.gif');height:10em; width:10em;background-repeat:no-repeat;}
+</style>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.js"></script>
+<?php
+session_start();
+$address = isset($_GET['address']) ? $_GET['address'] : '';
+$city = isset($_GET['city']) ? $_GET['city'] : 'New York';
+
+if ($address == ''):
+?>
+	<script>
+			$(function(){
+                                        $.ajax({url:"bing_test.php?address=<?php echo $address; ?>&city=<?php echo $city; ?>",
+                                        dataType:'text',
+                                        success:function(data){getlocalinfo(data.split(':'));}});
+			});
+
+function getlocalinfo(arrLatLon){
+					$.ajax({url:"foursquare_test.php?lat="+arrLatLon[0]+"&long="+arrLatLon[1], 
+					dataType:'html',
+					success:function(data){$('#foursquare').append(data); $('#loadingfoursquare').css('display', 'none');}});
+
+					$.ajax({url:"yipit_test.php?lat="+arrLatLon[0]+"&long="+arrLatLon[1],
+					dataType:'html',
+					success:function(data){$('#yipit').append(data); $('#loadingyipit').css('display', 'none');}});
+
+					$.ajax({url:"ordr_test.php", 
+					dataType:'html',
+					success:function(data){$('#ordr').append(data); $('#loadingordr').css('display', 'none');}});
+
+					$.ajax({url:"meetup_test.php?lat="+arrLatLon[0]+"&long="+arrLatLon[1],
+					dataType:'html',
+					success:function(data){$('#meetup').append(data); $('#loadingmeetup').css('display', 'none');}});
+}
+		</script>
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+
   </head>
 
   <body>
@@ -233,11 +213,15 @@ window.location.href = 'quinkennedy.com/reinventlocal/buildingly/index.php?city=
 </script>
 </head>
 <body style="background-image:url(assets/buildingly_launch_bg.png);">
-<div id="entry">
-<!--<img src="assets/buildingly_launch_box.png" />-->
-<input class="searchbar" type=text id="address" />
-<button class="searchbutton" style="position:absolute;" onclick="setaddress()" />
-</div>
+<img src="assets/buildingly_launch_box.png" style="display: block;   margin-left: auto;   margin-right: auto;" />
+
+        <div style="position:absolute; left:400px; top:235px; width: 700px;">
+             <div style="position:relative;">
+            <input size="100" class="searchbar" type="text" value="902 Broadway" />&nbsp;
+            <button class="searchbutton" style="position:absolute; left: 500px;" onclick="setaddress()"></button>
+             </div>
+
+        </div>
 </body>
 </html>
 <?php
